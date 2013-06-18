9410464727ab5efc21fd83d84baee6f3b3e932ab
Bartuz <iamnotawowplayer@gmail.com>
2013-06-18 16:39:00 -0400

    Progres is updating properly. input field works. working on enter key pressed handler

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 Array.prototype.randomElement = function () {
     return this[Math.floor(Math.random() * this.length)];
 };
+String.prototype.replaceAt=function(index, character) {
+    return this.substr(0, index) + character + this.substr(index+character.length);
+ }
 var words;
 var word;
 var temp;
@@ -19,16 +22,40 @@ var lives;
 	console.log("Started game with " + lives + " lives.")
 	console.log("Done!");
 })();
-function board(word){
-	var result = ""
-	for (var i=0;i<word.length;i++){
-		result+=(word[i] + " ")
+function drawBoard(){
+	console.log("Drawing board....")
+	var result = "";
+	for (var i=0;i<temp.length;i++){
+		result+=(temp[i] + " ");
 	}
-	return result
+	$("#status").text("Progress: " + result);
+	console.log("DONE!");
+}
+function updateTempWord(letter){
+	console.log("started updteding temp with letter:" + letter)
+	for (var i = 0; i < word.length;i++){
+		if (word[i]==letter){
+			console.log("Got it! " + temp[i] + " will be replaced with "  + letter)
+			temp= temp.replaceAt(i,letter);
+			console.log(temp);
+		}
+	}
+	console.log("done!")
+	drawBoard()
 }
 $(document).ready(function(){	
 	$("#debug").text("words is: " + word + " and was choosen from: '" + words.join("' / '") + "'");
-	$("#status").text("Progress: " + board(temp))
-	$("#lives").text("You have " + lives + " lives left!")
-	$("#guessed").text("Guessed: " + guessed.join(", "))
+	drawBoard();
+	$("#lives").text("You have " + lives + " lives left!");
+	$("#guessed").text("Guessed: " + guessed.join(", "));
+	$("input").on("keyup",function(keyPressed){
+		console.log(keyPressed)
+	});
+	$("button").on("click",function(){
+		console.log("Button event started....");
+		var letter = $("input").val();
+		console.log("Letter from input is: " + letter);
+		updateTempWord(letter);
+		$("input").val("");
+	});
 });
