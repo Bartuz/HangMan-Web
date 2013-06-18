c57855ec74e720a56f1c401ee2dfdebaa9f22099
Bartuz <iamnotawowplayer@gmail.com>
2013-06-18 15:03:11 -0400

    Printing board.

--- script.js
+++ script.js
@@ -6,14 +6,23 @@ var word;
 var temp;
 (function(){
 	console.log("Setting up variables...");
-	words = ["java","ruby"];
+	words = ["java","ruby","c plus plus","jscript","rails","html","css"];
 	word = words.randomElement();
 	console.log("Random word is: " + word);
-	temp = word
+	temp = word.replace(/[a-zA-Z]/g, "_")
+	console.log("Temp word is: " + temp)
 	console.log("Done!");
 
 })();
-
+function board(word){
+	var result = ""
+	for (var i=0;i<word.length;i++){
+		result+=(word[i] + " ")
+	}
+	return result
+}
 $(document).ready(function(){	
-	$("#status").text("choosen from: " + words.join(" + "));
+	$("#debug").text("words is: " + word + " and was choosen from: '" + words.join("' + '") + "'");
+	$("#status").text("Progress: " + board(temp))
+	$("")
 });
